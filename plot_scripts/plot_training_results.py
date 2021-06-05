9e9ebff19902007363c7d918707c190b23a1d294
halvorot <halvor1996@hotmail.com>
2021-06-05 09:42:36 +0200

    new plots

--- plot_scripts/plot_training_results.py
+++ plot_scripts/plot_training_results.py
@@ -60,7 +60,13 @@ def plot_ep_crash_mean(filepaths, labels=None, save=False):
         Y = df['Value']*100
         Y = smooth(Y,0.89)
         if labels:
-            ax.plot(X, Y, label=labels[i], zorder=i if i!=0 else 10)
+            if "with psf" in labels[i].lower():
+                if i>1:
+                    ax.plot(X, Y, label=labels[i], linestyle='dashed', dashes=(4, 3))
+                else:
+                    ax.plot(X, Y, label=labels[i], linestyle='dashed')
+            else:
+                ax.plot(X, Y, label=labels[i])
         else:
             ax.plot(X, Y, zorder=i)
         max_timesteps = np.maximum(max_timesteps, np.max(X))
@@ -70,7 +76,7 @@ def plot_ep_crash_mean(filepaths, labels=None, save=False):
     ax.set_ylabel(r'Crash rate mean')
     ax.set_xlim([0,max_timesteps])
     ax.grid(True)
-    ax.set_ylim([0,100])
+    ax.set_ylim([-5,100])
 
     formater = mtick.PercentFormatter(decimals=0)
     ax.yaxis.set_major_formatter(formater)
@@ -79,7 +85,7 @@ def plot_ep_crash_mean(filepaths, labels=None, save=False):
         ax.legend()
 
     if save:
-        plt.savefig('plots/ep_crash_mean_psf_10M.pdf', bbox_inches='tight')
+        plt.savefig('plots/ep_crash_mean_tight_constraints.pdf', bbox_inches='tight')
     
     plt.show()
 
@@ -126,6 +132,23 @@ if __name__ == '__main__':
                         r"C:\Users\halvorot\Downloads\run-VariableWindPSFtest-v17_1619696400ppo_tensorboard_PPO_1-tag-rollout_ep_rew_mean (2).csv"
                     ]
         filepaths = [filepaths[0],filepaths[7], filepaths[6], filepaths[13]]
+        filepaths = [   r"C:\Users\halvorot\Downloads\run-VariableWindLevel0-v17_1619804074ppo_tensorboard_PPO_1-tag-custom_crashed (2).csv",
+                        r"C:\Users\halvorot\Downloads\run-VariableWindLevel1-v17_1619817419ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",
+                        r"C:\Users\halvorot\Downloads\run-VariableWindLevel2-v17_1619805498ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",
+                        r"C:\Users\halvorot\Downloads\run-VariableWindLevel3-v17_1619826109ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",
+                        r"C:\Users\halvorot\Downloads\run-VariableWindLevel4-v17_1619817419ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",
+                        r"C:\Users\halvorot\Downloads\run-VariableWindLevel5-v17_1619809322ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",
+                    ]
+        filepaths = [   r"C:\Users\halvorot\Downloads\run-ConstantWindLevel1-v17_1620385846ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",
+                        r"C:\Users\halvorot\Downloads\run-ConstantWindLevel1-v17_1620385841ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",
+                        r"C:\Users\halvorot\Downloads\run-ConstantWindLevel2-v17_1620388963ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",                        
+                        r"C:\Users\halvorot\Downloads\run-ConstantWindLevel2-v17_1620385841ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",
+                    ]
+        filepaths = [   r"C:\Users\halvorot\Downloads\run-VariableWindLevel0-v17_1618915245ppo_tensorboard_PPO_1-tag-custom_crashed (3).csv",
+                        r"C:\Users\halvorot\Downloads\run-VariableWindLevel0-v17_1619804074ppo_tensorboard_PPO_1-tag-custom_crashed (4).csv",
+                        r"C:\Users\halvorot\Downloads\run-VariableWindPSFtest-v17_1619770390ppo_tensorboard_PPO_1-tag-custom_crashed (1).csv",                        
+                        r"C:\Users\halvorot\Downloads\run-VariableWindPSFtest-v17_1619696400ppo_tensorboard_PPO_1-tag-custom_crashed (2).csv",
+                    ]
     if args.label:
         labels = args.label
     else:
@@ -145,5 +168,23 @@ if __name__ == '__main__':
                     "Level HighWinds with PSF"
                     ]
         labels = [labels[0], labels[7], labels[6], labels[13]]
+        labels = [  "Level 0",
+                    "Level 1",
+                    "Level 2",
+                    "Level 3",
+                    "Level 4",
+                    "Level 5"
+        ]
+        labels = [  "Level ConstantLow",
+                    "Level ConstantLow with PSF",
+                    "Level ConstantHigh",
+                    "Level ConstantHigh with PSF"
+        ]
+        labels = [  "Level 0",
+                    "Level 0 with PSF",
+                    "Level HighWinds",
+                    "Level HighWinds with PSF"
+        ]
+        labels=None
     
-    plot_ep_rew_mean(filepaths, labels, save=args.save)
+    plot_ep_crash_mean(filepaths, labels, save=args.save)
